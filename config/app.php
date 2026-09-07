c6378295a93e0ffe37d481c33a3b463a40e5752e
Fandie-yombisse <yombissefandie@gmail.com>
2026-09-07 22:40:20 +0000

    preparation de l hebergements

--- config/app.php
+++ config/app.php
@@ -54,6 +54,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'force_https' => (bool) env('FORCE_HTTPS', false),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
